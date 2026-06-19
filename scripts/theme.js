50323a5bed57800b9a4f11ff78da272e78d0a132
Marta Kule <mkule@ebay.com>
2026-06-19 16:25:06 -0400

    switch theme to system, start work on button

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,5 +1,40 @@
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-console.log("systemSettingDark ", systemSettingDark.matches);
+const stored = localStorage.getItem("theme");
+const system = window.matchMedia("(prefers-color-scheme: dark)").matches
+	? "dark"
+	: "light";
+const fallback = "light";
 
-const systemSettingLight = window.matchMedia("(prefers-color-scheme: light)");
-console.log("systemSettingLight ", systemSettingLight.matches);
+const theme = stored || system || fallback;
+
+// On page load, set the theme
+document.querySelector("html").setAttribute("data-theme", theme);
+
+// Return props for the icon button
+const getButtonProps = () => {
+	if (theme === "light") {
+		return {
+			icon: "./assets/sun.svg",
+			label: "light theme",
+		};
+	}
+	if (theme === "dark") {
+		return {
+			icon: "./assets/moon.svg",
+			label: "dark theme",
+		};
+	}
+	return {
+		icon: "./assets/computer.svg",
+		label: "system theme",
+	};
+};
+
+export const getThemeButton = () => ({
+	...getButtonProps(),
+	hide: false,
+	onClick: handleThemeChange,
+});
+
+function handleThemeChange() {
+	return 2;
+}
